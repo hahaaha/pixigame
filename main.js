63d399f212b87eab9d96e6d06ca71ae312f3119b
haha <390519764@qq.com>
2022-10-24 23:51:27 +0800

    feat: add map add paly

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-import {Application, Container, Loader, Sprite,utils, Rectangle} from 'pixi.js'
-const app = new Application({width: 256, height: 256})
+import { Application, Container, Loader, Sprite, utils, Rectangle } from 'pixi.js'
+const app = new Application({ width: 792, height: 670 })
 const loader = new Loader()
-const {TextureCache} = utils
-// document.body.appendChild(app.view)
+const { TextureCache } = utils
+let dungeon, explorer, treasure, id;
 
-
-// loader.add('assets/09.png').load(setup)
+document.body.appendChild(app.view)
+loader.add('assets/sp.json').load(setup)
 
 function setup() {
-  const gameScene = new Container()
-  app.stage.appChild(gameScene)
-  const gameOverScene = new Container()
-  app.stage.appChild(gameOverScene)
-  gameOverScene.visible = false
+  id = loader.resources['assets/sp.json'].textures;
+  dungeon = new Sprite(id["map.png"]);
+  app.stage.addChild(dungeon);
+  treasure = new Sprite(id['characters-4.png'])
+  treasure.x = 100;
+  treasure.y = app.stage.height / 2 ;
+  app.stage.addChild(treasure)
+  // const gameScene = new Container()
+  // app.stage.appChild(gameScene)
+  // const gameOverScene = new Container()
+  // app.stage.appChild(gameOverScene)
+  // gameOverScene.visible = false
 
 
-  state = play
-  app.ticker.add(delta => gameLoop(delta))
+  // state = play
+  // app.ticker.add(delta => gameLoop(delta))
 }
 
 
 function gameLoop(delta) {
   //Runs the current game `state` in a loop and renders the sprites
- }
- 
- function play(delta) {
+}
+
+function play(delta) {
   //All the game logic goes here
- }
- 
- function end() {
+}
+
+function end() {
   //All the code that should run at the end of the game
- }
+}
 
